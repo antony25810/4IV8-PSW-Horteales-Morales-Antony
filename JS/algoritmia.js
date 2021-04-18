922ddd3499a51fd95585a8dcf604e3a27875ef2a
antony horteales <ahortealesm1900@alumno.ipn.mx>
2021-04-17 22:51:06 -0500

    ya no se aceptan minusculas

--- JS/algoritmia.js
+++ JS/algoritmia.js
@@ -70,6 +70,16 @@ function problema2() {
 
 }
 
+function validarn(e){
+    var teclado = (document.all)?e.keyCode:e.which;
+    if(teclado == 8)return true;
+
+    var patron = /[0-9\d .]/;
+
+    var prueba = String.fromCharCode(teclado);
+    return patron.test(prueba);
+}
+
 function problema3() {
     var p3_input = document.querySelector('#p3-input').value;
     var longitud;
